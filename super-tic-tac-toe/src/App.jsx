ae700bb86773c2c2c1a0b52695ebbdc6913c321f
Akhand <akhand99@gmail.com>
2025-04-06 00:05:16 +0530

    Refactored App component.

--- super-tic-tac-toe/src/App.jsx
+++ super-tic-tac-toe/src/App.jsx
@@ -1,21 +1,14 @@
 import { useEffect, useState } from "react";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
 import "./css/styles1.css";
 import MainBoard from "./components/MainBoard";
-import X_Avatar from "./assets/X_Avatar.png";
-import O_Avatar from "./assets/O_Avatar.png";
-import Hamburger_White from "./assets/menu_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg";
-import HamburgerMenu from "./components/HamburgerMenu";
+import AvatarHamburgerContainer from "./components/AvatarHamburgerContainer";
 
 function App() {
   const [currentMarkerToPlace, setCurrentMarkerToPlace] = useState("X");
   const [isInitialRender, setIsInitialRender] = useState(true);
-  const [isHamburgerMenuVisible, setIsHamburgerMenuVisible] = useState(false);
-  const [resetCount, setResetCount] = useState(0);
   // Adding resetcount state here and passing it as key prop to
-  // MainBoard so that, we can reset whole game state by re-mounting it.
+  // MainBoard so that, we can reset whole game state by re-mounting it.... for now.
+  const [resetCount, setResetCount] = useState(0);
 
   useEffect(() => {
     if (isInitialRender) {
@@ -30,41 +23,14 @@ function App() {
     oTurnIndicatorClsName = currentMarkerToPlace === "O" ? "visible" : "hidden";
   }
 
-  function handleHamburgerClick() {
-    setIsHamburgerMenuVisible(!isHamburgerMenuVisible);
-  }
-
   return (
     <>
-      {/* <h1>Super Tic-Tac-Toe</h1> */}
-      <div className="avatar-hamburger-container">
-        <img
-          src={X_Avatar}
-          alt="X-Avatar"
-          id="player-x-avatar"
-          className={currentMarkerToPlace === "X" ? "" : "dim"}
-        ></img>
-        <div
-          style={{
-            display: "flex",
-            gap: "12px",
-            alignItems: "center",
-            paddingRight: "18px",
-          }}
-        >
-          <img
-            src={O_Avatar}
-            alt="O-Avatar"
-            id="player-o-avatar"
-            className={currentMarkerToPlace === "O" ? "" : "dim"}
-          ></img>
-          <img
-            src={Hamburger_White}
-            style={{ height: "36px" }}
-            onClick={handleHamburgerClick}
-          ></img>
-        </div>
-      </div>
+      <AvatarHamburgerContainer
+        currentMarkerToPlace={currentMarkerToPlace}
+        setResetCount={setResetCount}
+        setIsInitialRender={setIsInitialRender}
+        setCurrentMarkerToPlace={setCurrentMarkerToPlace}
+      ></AvatarHamburgerContainer>
       <div
         className={`turn-indicator-div x-turn ${
           currentMarkerToPlace === "X" ? "visible" : "hidden"
@@ -74,14 +40,6 @@ function App() {
         className={`turn-indicator-div o-turn ${oTurnIndicatorClsName}`}
         style={{ display: `${isInitialRender ? "none" : "unset"}` }}
       ></div>
-      {isHamburgerMenuVisible ? (
-        <HamburgerMenu
-          setResetCount={setResetCount}
-          setIsHamburgerMenuVisible={setIsHamburgerMenuVisible}
-          setIsInitialRender={setIsInitialRender}
-          setCurrentMarkerToPlace={setCurrentMarkerToPlace}
-        ></HamburgerMenu>
-      ) : null}
       <MainBoard
         key={resetCount}
         currentMarkerToPlace={currentMarkerToPlace}
---
+++ super-tic-tac-toe/src/components/AvatarHamburgerContainer.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import X_Avatar from "../assets/X_Avatar.png";
+import O_Avatar from "../assets/O_Avatar.png";
+import Hamburger_White from "../assets/menu_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg";
+import HamburgerMenu from "./HamburgerMenu";
+
+export default function AvatarHamburgerContainer({
+  currentMarkerToPlace,
+  setResetCount,
+  setIsInitialRender,
+  setCurrentMarkerToPlace,
+}) {
+  const [isHamburgerMenuVisible, setIsHamburgerMenuVisible] = useState(false);
+
+  function handleHamburgerClick() {
+    setIsHamburgerMenuVisible(!isHamburgerMenuVisible);
+  }
+
+  return (
+    <>
+      <div className="avatar-hamburger-container">
+        <img
+          src={X_Avatar}
+          alt="X-Avatar"
+          id="player-x-avatar"
+          className={currentMarkerToPlace === "X" ? "" : "dim"}
+        ></img>
+        <div
+          style={{
+            display: "flex",
+            gap: "12px",
+            alignItems: "center",
+            paddingRight: "18px",
+          }}
+        >
+          <img
+            src={O_Avatar}
+            alt="O-Avatar"
+            id="player-o-avatar"
+            className={currentMarkerToPlace === "O" ? "" : "dim"}
+          ></img>
+          <img
+            src={Hamburger_White}
+            style={{ height: "36px" }}
+            onClick={handleHamburgerClick}
+          ></img>
+        </div>
+      </div>
+      {isHamburgerMenuVisible ? (
+        <HamburgerMenu
+          setResetCount={setResetCount}
+          setIsHamburgerMenuVisible={setIsHamburgerMenuVisible}
+          setIsInitialRender={setIsInitialRender}
+          setCurrentMarkerToPlace={setCurrentMarkerToPlace}
+        ></HamburgerMenu>
+      ) : null}
+    </>
+  );
+}
